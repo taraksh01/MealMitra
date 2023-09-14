c1086266bf9e1d0f220542146d358b8326ca07e8
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-14 12:20:11 +0530

    Created basic app and rendered it to the page

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <h1>MealMitra</h1>;
+};
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(<App />);
