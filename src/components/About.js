786f4ea4b14d158dda8484a7e9f6a46ea164c30d
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-16 12:00:23 +0530

    Created about and contact for testing react router

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="about">
+      <h1>This is the about page for testing router</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1>This is contact page for testing router</h1>
+    </div>
+  );
+};
+
+export default Contact;
