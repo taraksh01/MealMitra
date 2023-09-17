7789601e8bc0ed3506c812ebf7669db8143b437d
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-17 11:09:43 +0530

    Displayed all the api data to the page

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -30,7 +30,59 @@ const RestaurantMenu = () => {
     totalRatingsString,
   } = resInfo[0]?.card?.card?.info;
 
-  return <div className="restaurant-menu">RestaurantMenu</div>;
+  const offers = resInfo[1]?.card?.card?.gridElements?.infoWithStyle?.offers;
+
+  const menus = resInfo[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+
+  return (
+    <div className="restaurant-menu">
+      <div className="restaurant-card">
+        <div className="detail">
+          <h2 className="name">{name}</h2>
+          <p>{cuisines.join(",")}</p>
+          <p>{areaName}</p>
+          <p>{feeDetails.message}</p>
+          <p>{costForTwoMessage}</p>
+        </div>
+        <div className="rating">
+          <p>{avgRating}</p>
+          <p>{totalRatingsString}</p>
+        </div>
+      </div>
+
+      <div className="offers-menu">
+        {offers?.map((offer, index) => (
+          <div className="offer" key={index}>
+            <h3 className="offers-title">{offer?.info?.header}</h3>
+            <p className="coupon-code">{offer?.info?.couponCode}</p>
+            <p className="description">{offer?.info?.description}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="menu">
+        {menus
+          ?.filter((menu, index) => index > 0 && index < menus.length - 1)
+          ?.map((menu, index) => (
+            <div className="menu" key={index}>
+              <h1 className="menus-title">{menu?.card?.card?.title}</h1>
+              <div>
+                {menu?.card?.card?.itemCards?.map((item) => (
+                  // console.log(item?.card?.info)
+                  <div className="item" key={item?.card?.info?.id}>
+                    <h4>{item?.card?.info?.name}</h4>
+                    <p>{item?.card?.info?.price}</p>
+                    <p>{item?.card?.info?.category}</p>
+                    <p>{item?.card?.info?.description}</p>
+                    <p>{item?.card?.info?.itemAttribute?.vegClassifier}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantMenu;
