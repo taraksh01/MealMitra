f484f9f176858c4ddb80af76a5339befceadc373
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-17 11:11:50 +0530

    Added MENU_API_URL as constant

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,7 @@
 const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/";
 
-export { CDN_URL };
+const MENU_API_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+
+export { CDN_URL, MENU_API_URL };
