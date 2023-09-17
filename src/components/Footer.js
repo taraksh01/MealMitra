39c1a1339590daff7fbc03daada7483cdd0307ee
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-17 11:32:30 +0530

    Added link class to all the links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,24 @@ const Footer = () => {
       <div>
         <ul className="container">
           <li>
-            <Link to={"/about"}>About</Link>
+            <Link className="link" to={"/about"}>
+              About
+            </Link>
           </li>
           <li>
-            <Link to={"/contact"}>Contact</Link>
+            <Link className="link" to={"/contact"}>
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
       <div className="credit">
         created by
-        <Link to={"https://github.com/taraksh01/"} target="_blank">
+        <Link
+          className="link"
+          to={"https://github.com/taraksh01/"}
+          target="_blank"
+        >
           <span className="username">taraksh01</span>
         </Link>
       </div>
